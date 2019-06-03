20ca8916f9bb6fab7ab3a56c334267d7d6d757f1
cccen <cccen@163.com>
2019-06-03 03:07:56 +0000

    登录成功后导航逻辑

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,16 +5,14 @@
     <link rel="stylesheet" href="{{mix('css/app.css')}}">
 </head>
 <body>
-    @include('layouts._header')
-
-
+@include('layouts._header')
 <div class="container">
     <div class="offset-md-1 col-md-10">
         @include('shared._messages')
         @yield('content')
         @include('layouts._footer')
     </div>
-
+    <script src="{{mix('js/app.js')}}"></script>
 </div>
 </body>
 </html>
