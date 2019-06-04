379d395a3c2ab72779ca1081239e523277b4051b
cccen <cccen@163.com>
2019-06-04 08:06:54 +0000

    访问策略

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -6,6 +6,13 @@ use Illuminate\Http\Request;
 
 class SessionController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('guest',[
+            'only'=>['create']
+        ]);
+    }
+
     public function create(){
         return view('sessions.create');
     }
@@ -18,11 +25,22 @@ class SessionController extends Controller
 
         if(Auth::attempt($credentials)){
             session()->flash('success','欢迎回来');
-            return redirect()->route('users.show',[Auth::user()]);
+            $fallback=route('users.show',Auth::user());
+            return redirect()->intended($fallback);
+            //return redirect()->route('users.show',[Auth::user()]);
         }else{
             session()->flash('danger','抱歉，登录失败');
             return redirect()->back()->withInput();
         }
 
     }
+
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success','您已成功退出！');
+        return redirect('login');
+    }
+
+
 }
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,29 +8,68 @@ use Auth;
 
 class UsersController extends Controller
 {
-    public function create(){
+    public function __construct()
+    {
+        $this->middleware('auth',[
+            'except'=>['show','create','store']
+        ]);
+
+        $this->middleware('guest',[
+            'only'=>['create']
+        ]);
+    }
+
+    public function create()
+    {
         return view('users.create');
     }
 
-    public function show(User $user){
-        return view('users.show',compact('user'));
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
     }
 
-    public function store(Request $request){
-        $this->validate($request,[
-            'name'=>'required|max:50',
-            'email'=>'required|email|unique:users|max:255',
-            'password'=>'required|confirmed|min:6'
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
         ]);
 
-        $user=User::create([
-            'name'=>$request->name,
-            'email'=>$request->email,
-            'password'=>bcrypt($request->password),
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
         ]);
 
         Auth::login($user);
-        session()->flash('success','欢迎，您将在这里开始一段新的旅程');
-        return redirect()->route('users.show',[$user]);
+        session()->flash('success', '欢迎，您将在这里开始一段新的旅程');
+        return redirect()->route('users.show', [$user]);
+    }
+
+    public function edit(User $user)
+    {
+        $this->authorize('update',$user);
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(User $user, Request $request)
+    {
+        $this->authorize('update',$user);
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => '->nullable|confirmed|min:6'
+        ]);
+
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+
+        session()->flash('success','个人资料更新成功！');
+        return redirect()->route('users.show', $user);
     }
 }
