ad237f09bdc03268da070263c6e94856509f0087
cccen <cccen@163.com>
2019-06-13 03:29:54 +0000

    删除微博

--- resources/views/statuses/_status.blade.php
+++ resources/views/statuses/_status.blade.php
@@ -1,9 +1,17 @@
 <li class="media mt-4 mb-4">
-    <a href="{{route('users.show',$user->id)}}">
-        <img src="{{$user->gravatar()}}" alt="{{$user->name}}" class="mr-3 gravatar"/>
+    <a href="{{ route('users.show', $user->id )}}">
+        <img src="{{ $user->gravatar() }}" alt="{{ $user->name }}" class="mr-3 gravatar"/>
     </a>
     <div class="media-body">
-        <h5 class="mt-0 mb-1">{{$user->name}}<small> / {{$status->created_at->diffForHumans()}}</small></h5>
-        {{$status->content}}
+        <h5 class="mt-0 mb-1">{{ $user->name }} <small> / {{ $status->created_at->diffForHumans() }}</small></h5>
+        {{ $status->content }}
     </div>
+
+    @can('destroy', $status)
+        <form action="{{ route('statuses.destroy', $status->id) }}" method="POST" onsubmit="return confirm('确认要删除？');">
+            {{ csrf_field() }}
+            {{ method_field('DELETE') }}
+            <button type="submit" class="btn btn-sm btn-danger">删除</button>
+        </form>
+    @endcan
 </li>
