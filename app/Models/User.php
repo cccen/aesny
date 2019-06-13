c68c4696df96f82cff1364296d64086ff125a003
cccen <cccen@163.com>
2019-06-13 01:30:30 +0000

    微博数据模型

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,4 +50,9 @@ class User extends Authenticatable
             $user->activation_token = Str::random(10);
         });
     }
+
+    public function statues()
+    {
+        return $this->hasMany(Status::class);
+    }
 }
